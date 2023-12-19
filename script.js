acef5411a651e61251c8008baec33ad7e4a126a6
ddsdharan <128017667+ddsdharan@users.noreply.github.com>
2023-12-20 01:02:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function openModal() {
+    var modal = new bootstrap.Modal(document.getElementById('myModal'));
+    modal.show();
+
+    document.getElementById('notifyForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        var fullName = document.getElementById('fullName').value;
+        var email = document.getElementById('email').value;
+
+        fetch('http://localhost:8000/api/submitFormData', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ fullName: fullName, email: email }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Form data submitted to server:', data);
+                modal.hide();
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+}
